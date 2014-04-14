6e8a422d27f1096b8e0d2a4b2aa04a58c898a1ea
skv-headless <skv-headless@yandex.ru>
2014-04-14 20:43:46 +0400

    description and / operator

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 my_own_lisp
 ===========
+
+**compile**
+cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing
+
+**start**
+./parsing
--- parsing.c
+++ parsing.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPC_LANG_DEFAULT, 
       "                                                     \
         number   : /-?[0-9]+/ ;                             \
-        operator : '+' | '-' | '*' | ;                      \
+        operator : '+' | '-' | '*' | '/';                   \
         expr     : <number> | '(' <operator> <expr>+ ')';   \
         lispy    : /^/ <operator> <expr>+ /$/ ;             \
       ",
