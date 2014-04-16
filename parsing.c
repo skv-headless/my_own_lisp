c76de5c551dec94ff0d1350b72dc12d7bce4fc96
skv-headless <skv-headless@yandex.ru>
2014-04-16 22:52:56 +0400

    eval

--- parsing.c
+++ parsing.c
@@ -6,7 +6,32 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
-static char input[2048];
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
@@ -29,12 +54,13 @@ int main(int argc, char** argv) {
   while(1) {
     char* input = readline("lispy> ");
     add_history(input);
-    
+
     /* Attempt to Parse the user Input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On Success Print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise Print the Error */
@@ -44,7 +70,8 @@ int main(int argc, char** argv) {
 
     free(input);
   }
-  
+
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
+
